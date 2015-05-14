faf42c797e242fc238a4b64fdd1c09d1b184ddbf
BloodAxe <ekhvedchenya@gmail.com>
2015-05-14 09:11:42 +0300

    Fixed bug that prevented CORS headers to appear in a response

--- server.js
+++ server.js
@@ -66,9 +66,11 @@ function MapAnalyzeResult(analyzeResult) {
 }
 
 // Peace to everyone!
-app.all('/api/', function(req, res, next) {
+
+app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    
     next();
 });
 
